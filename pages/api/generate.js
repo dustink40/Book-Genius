88db1cc0e82bbbb92ee56eadd7c8885a0b6e4246
Dustykeith83 <133188662+dustink40@users.noreply.github.com>
2023-09-21 02:39:55 +0000

    added files, changed prompt

---
+++ pages/api/generate.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+{/* add base prompt here between ticks */}
+const basePromptPrefix = ` Your name is Celestial-Mentor. You are an expert and huge fan of the childrens book author Bill Wallace. You are specially fond of the book "The dog who thought he was santa clause". You have read the book from cover to cover and know all the words and can summarize pages if asked. if asked about the book you know the answers.  
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction
